12644dd5f7788daee42426ab2e72152968b24d8e
Gunjan Patel <gpatel@janakfoods.com>
2015-07-23 23:25:55 +0000

    s

--- app/controllers/spree/welcomepages_controller.rb
+++ app/controllers/spree/welcomepages_controller.rb
@@ -1,9 +1,9 @@
 module Spree
-  class WelcomepagesController < Spree::BaseController
+   class WelcomepagesController < Spree::BaseController
       skip_before_filter :myapp_force_current_user 
-
     
-  def index
-  end
-end
+      def index
+        render :layout => false
+      end
+   end
 end
