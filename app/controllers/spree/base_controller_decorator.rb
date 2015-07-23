c58a34dd5bcaaedefcfa8faef784a592c0a245ca
Gunjan Patel <gpatel@janakfoods.com>
2015-07-23 23:02:46 +0000

    s

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -2,11 +2,11 @@ Spree::BaseController.class_eval do
   # The next line will not work, will cause redirect loop 
   # before_filter :authenticate_user! 
 
- before_filter :myapp_force_current_user 
+  before_filter :myapp_force_current_user 
 
   def myapp_force_current_user 
     unless spree_current_user 
-      redirect_to login_url and return 
+      redirect_to welcomepages_index_url and return 
     end 
   end 
 end 
