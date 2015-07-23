f2b37e168f60ad8abdad0a632b234da3e772e715
Gunjan Patel <gpatel@janakfoods.com>
2015-07-23 03:07:51 +0000

    s

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-    get 'welcomepages/index' => 'welcomepages#index'
-
 end
+    get 'welcomepages/index' => 'welcomepages#index'
