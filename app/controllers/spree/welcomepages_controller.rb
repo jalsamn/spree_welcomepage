1ed94176949c69eefbcc5edfd430a2e8faac78e9
Gunjan Patel <gpatel@janakfoods.com>
2015-09-08 03:21:51 +0000

    s

--- app/controllers/spree/welcomepages_controller.rb
+++ app/controllers/spree/welcomepages_controller.rb
@@ -1,9 +1,25 @@
 module Spree
    class WelcomepagesController < Spree::BaseController
-      skip_before_filter :myapp_force_current_user 
+    #  skip_before_action :myapp_force_current_user
+     skip_filter *_process_action_callbacks.map(&:filter)
+
+      layout false
     
       def index
-        render :layout => false
+      end
+     
+       def zipvalidation
+         if Spree::Areamanager.exists?(:zipcode=>params[:query])
+           redirect_to signup_path
+         else
+           redirect_to welcomepages_zipnotavailable_path
+         end
+       end
+     
+     def zipnotavailable
+       end
+     
+     def register
       end
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
     get 'welcomepages/index' => 'welcomepages#index'
+    get 'welcomepages/zipvalidation' => 'welcomepages#zipvalidation'
+    get 'welcomepages/zipnotavailable' => 'welcomepages#zipnotavailable'
 end
